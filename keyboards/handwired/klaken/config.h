87cbb89ea8b26d5ef9a5f93de0d71ada305a3461
Seth Lienemann <sethlienemann@gmail.com>
2024-07-04 17:47:48 -0500

    Cleanup code.

--- keyboards/handwired/klaken/config.h
+++ keyboards/handwired/klaken/config.h
@@ -14,9 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #pragma once
 
-//#define USE_SERIAL
-
-#define MASTER_LEFT
+#include "config_common.h"
